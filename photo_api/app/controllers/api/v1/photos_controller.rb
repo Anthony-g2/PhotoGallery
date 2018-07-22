51e6f9cb4bb278d3d574082efaeeccd8081d29e2
Anthony-g2 <ant.greenwell2@gmail.com>
2018-07-21 19:25:00 -0700

    clientside created

---
+++ photo_api/app/controllers/api/v1/photos_controller.rb
@@ -0,0 +1,11 @@
+module Api::V1
+
+  class PhotosController < ApplicationController
+
+    def index
+      @photos = Photo.all
+      render json: @photos
+    end
+
+  end
+end
--- photo_api/db/seeds.rb
+++ photo_api/db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+photos = Photo.create(
+  [
+    {
+      url: 'https://timedotcom.files.wordpress.com/2015/09/daenerys-game-of-thrones-2.jpg?quality=85',
+      desc: 'Mother of Dragons',
+    },
+    {
+      url: 'https://nerdist.com/wp-content/uploads/2017/06/drogon-and-dany-game-of-thrones-featured.jpg',
+      desc: 'Dracarys',
+    },
+    {
+      url: 'https://images.immediate.co.uk/volatile/sites/3/2017/08/promo320627889.jpeg?quality=45&crop=25px,0px,3153px,2100px&resize=620,413',
+      desc: 'Night King',
+    },
+    {
+      url: 'https://i2-prod.mirror.co.uk/incoming/article10883698.ece/ALTERNATES/s615/Game-of-Thrones.jpg',
+      desc: 'He\'s King of the North',
+    },
+    {
+      url: 'https://cimg.tvgcdn.net/i/r/2018/02/01/8c06b694-82fa-45ce-a93a-196aaca1f0a0/crop/1380x1380+345+0/thumbnail/300x300/d6e553d328c997d4044015ce12935784/180201-game-of-thrones.jpg',
+      desc: 'The Stark Children Reunited',
+    },
+  ]
+)
